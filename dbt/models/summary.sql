f19201e0df4355f05b7353f22e281cdcd8d609e0
DevSmapy <lyk456@naver.com>
2026-05-26 17:57:46 +0900

    Add `total_market_count` metric to `summary.sql` model

--- dbt/models/summary.sql
+++ dbt/models/summary.sql
@@ -1,6 +1,7 @@
 select
     count(*) as total_row_count,
     count(distinct Ticker) as total_ticker_count,
+    count(distinct Market) as total_market_count,
     min(Date) as first_date,
     max(Date) as last_date
 from {{ ref('stg_raw_stocks') }}
